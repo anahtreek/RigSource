3148278f5c52f7a0301287e8e91650c6e81af62e
THREKASHRI <threka1127@gmail.com>
2020-10-27 01:55:46 +0530

    Update basecode.sh

--- basecode.sh
+++ basecode.sh
@@ -1,6 +1,7 @@
-sudo rm -rf .git
+sudo mkdir $5
 sudo git init
 sudo git remote add origin https://$1:$2@dev.azure.com/$3/$4/_git/$5
 sudo git add .
 sudo git commit -m 'initial commit'
 sudo git push -u origin master
+Sudo rm -rf $5
